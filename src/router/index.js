1eef2868afc9f06053074dfe1a8ebaeb86e96e88
unknown <chromeee@daum.net>
2022-02-01 16:41:01 +0900

    로그인 인증 라우터 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
+import store from "@/store";
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/login',
-  },
-  {
-    path: '/signup',
-    name: 'SignupPage',
-    component: () => import('../views/SignupPage.vue'),
-  },
-  {
-    path: '/login',
-    name: 'LoginPage',
-    component: () => import('@/views/LoginPage.vue'),
-  },
-  {
-    path: '/main',
-    name: 'MainPage',
-    component: () => import('@/views/MainPage.vue'),
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFoundPage',
-    component: () => import('@/views/NotFoundPage.vue'),
-  },
+    {
+        path: "/",
+        redirect: "/login",
+    },
+    {
+        path: "/signup",
+        name: "SignupPage",
+        component: () => import("../views/SignupPage.vue"),
+    },
+    {
+        path: "/login",
+        name: "LoginPage",
+        component: () => import("@/views/LoginPage.vue"),
+    },
+    {
+        path: "/main",
+        name: "MainPage",
+        component: () => import("@/views/MainPage.vue"),
+        meta: { auth: true },
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFoundPage",
+        component: () => import("@/views/NotFoundPage.vue"),
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    //라우터의 meta.auth가 true이고 getters에 있는 isLogin이 false일 경우 로그인페이지로 이동
+    if (to.meta.auth && !store.getters.isLogin) {
+        alert("로그인이 필요합니다.");
+        next("/login");
+        return;
+    }
+    next();
 });
 
 export default router;
